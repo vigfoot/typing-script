8841a02e6a215f3146a2c5353b9872f7562930da
Xasquatch <bigfoot7774@gmail.com>
2024-02-11 02:25:19 +0900

    Initial commit

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 const tcp = {
     option: {
-        header: {},
+        headers: {},
         async: false,
         method: null,
         url: null,
-        body: null,
+        body: {},
     },
     ajax: function () {
         let xhr = new XMLHttpRequest();
+        if (!vig.isNull(tcp.option.method)
+            && tcp.option.method.trim().toUpperCase() === 'GET'
+            && !vig.isNull(tcp.option.body)){
+
+            tcp.option.url += '?';
+            for (const key in tcp.option.body) {
+                tcp.option.url += 'key=' + tcp.option.body[key] + '&';
+            }
+            tcp.option.body = {};
+        }
+
         xhr.open(tcp.option.method, tcp.option.url, tcp.option.async);
-        if (!tcp.isNull(tcp.option.header)){
-            for (const headerKey in tcp.option.header) {
-                let headerValue = tcp.option.header[headerKey];
+        if (!vig.isNull(tcp.option.headers)) {
+            for (const headerKey in tcp.option.headers) {
+                let headerValue = tcp.option.headers[headerKey];
                 xhr.setRequestHeader(headerKey, headerValue);
             }
         }
         xhr.send(tcp.option.body);
     },
     fetch: function () {
+        if (tcp.option.async === false) return tcp.ajax();
 
+        return fetch(tcp.option.url, {
+            headers: tcp.option.headers,
+            method: tcp.option.method,
+            body: tcp.option.body
+        }).then(value => value.json())
     },
+}
+
+const vig = {
     isNull: function (value) {
         if (value === null || value === undefined)
             return true;
@@ -30,5 +50,5 @@ const tcp = {
         if (value instanceof Object && Object.keys(value).length === 0)
             return false;
         return false;
-    }
+    },
 }
